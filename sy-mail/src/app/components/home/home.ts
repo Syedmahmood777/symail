ffbe70f525c848f09f6928cb4be40de9dbf97255
Syedmahmood777 <syedmahmood717@gmail.com>
2025-08-29 16:59:13 +0530

    add about section

--- sy-mail/src/app/components/home/home.ts
+++ sy-mail/src/app/components/home/home.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { trigger, transition, style, animate, query, stagger } from '@angular/animations';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './home.html',
-  styleUrl: './home.scss'
-})
-export class Home {
+  styleUrls: ['./home.scss'],
+  animations: [
+    trigger('boxAnimation', [
+  transition(':enter', [
+    query(
+      '.workflow-box',
+      [
+        style({ opacity: 0, transform: 'translateY(30px)' }),
+        stagger(400, [
+          animate(
+            '1700ms ease-out',
+            style({ opacity: 1, transform: 'translateY(0)' })
+          )
+        ])
+      ],
+      { optional: true } // <<< critical, prevents null crash
+    )
+  ])
+])
 
-}
+  ]
+})
+export class Home {}
