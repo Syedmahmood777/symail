d170f40a7f079ff8f96b1c30d22916cbdf0072ba
Syedmahmood777 <syedmahmood717@gmail.com>
2025-09-01 16:44:45 +0530

    fix routing #2

--- sy-mail/src/app/components/home/home.spec.ts
+++ sy-mail/src/app/components/home/home.spec.ts
@@ -1,14 +1,15 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { Home } from './home';
-
+import { provideRouter } from '@angular/router';
 describe('Home', () => {
   let component: Home;
   let fixture: ComponentFixture<Home>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Home]
+      imports: [Home],
+      providers: [provideRouter([])]  
     })
     .compileComponents();
 
