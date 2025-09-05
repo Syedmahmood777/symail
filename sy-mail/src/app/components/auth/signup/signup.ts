08ced8d1eabd1933f090f1a0f73b8f8b002e5034
Syedmahmood777 <syedmahmood717@gmail.com>
2025-09-05 16:50:16 +0530

    create formgroup

--- sy-mail/src/app/components/auth/signup/signup.ts
+++ sy-mail/src/app/components/auth/signup/signup.ts
@@ -1,11 +1,19 @@
 import { Component } from '@angular/core';
+import { FormGroup,FormControl,Validators,ReactiveFormsModule,FormControlName} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './signup.html',
   styleUrl: './signup.scss'
 })
-export class Signup {
 
+export class Signup {
+  form = new FormGroup({
+    firstn:new FormControl('',Validators.required),
+    lastn:new FormControl('',Validators.required),
+    email: new FormControl('', Validators.email),
+    password: new FormControl('', Validators.minLength(6))
+
+  });
 }
