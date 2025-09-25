6e6266e2f6559d2dce872f495c6734b7ee589109
Syedmahmood777 <syedmahmood717@gmail.com>
2025-09-25 15:12:50 +0530

    add login form

--- sy-mail/src/app/components/auth/login/login.ts
+++ sy-mail/src/app/components/auth/login/login.ts
@@ -1,11 +1,27 @@
-import { Component } from '@angular/core';
-
+import { Component, ElementRef, ViewChild, HostListener } from '@angular/core';
+import {
+  FormGroup,
+  FormControl,
+  Validators,
+  ReactiveFormsModule,
+  AbstractControl,
+  ValidationErrors,
+} from '@angular/forms';
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss'
+  styleUrl: './login.scss',
 })
 export class Login {
-
+  form = new FormGroup({
+    email: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^[a-zA-Z0-9._%+-]+@symail\.co$/),
+    ]),
+    pass: new FormControl('', [
+      Validators.required,
+      Validators.pattern(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[\W_]).{9,}$/),
+    ]),
+  });
 }
