87eba9a1465afb0b6f4bf6d17ad688808e6f4318
Syedmahmood777 <syedmahmood717@gmail.com>
2025-10-08 16:53:37 +0530

    add views and urls for django auth

--- server/auth_app/models.py
+++ server/auth_app/models.py
@@ -12,3 +12,21 @@ class Auth(models.Model):
 
     def __str__(self):
         return self.email
+class Info(models.Model):
+    id = models.BigAutoField(primary_key=True)  
+    email = models.EmailField(max_length=255)
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    country = models.CharField(max_length=30)
+    city = models.CharField(max_length=30)
+    phone = models.BigIntegerField()
+    address = models.TextField()      
+    pin = models.CharField(max_length=10);                       
+    date_joined = models.DateTimeField()              
+
+    class Meta:
+        managed = False                              
+        db_table = 'info'       
+
+    def __str__(self):
+        return self.email
---
+++ server/auth_app/views.py
@@ -0,0 +1,55 @@
+from django.http import JsonResponse
+from django.utils import timezone
+from .models import Auth,Info
+from django.views.decorators.csrf import csrf_exempt
+import json
+
+@csrf_exempt
+def signup(request):
+     if request.method == 'POST':
+
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+        fname = data.get('fname')
+        lname = data.get('lname')
+        country = data.get('country')
+        city = data.get('city')
+        phone = data.get('phone')
+        address = data.get('address')
+        pin=data.get('pin')
+        date_joined=timezone.now()
+
+        
+        
+        
+        try:
+            Auth.objects.create(email=email, password=password,date_joined=date_joined)
+            Info.objects.create(email=email, fname=fname, lname=lname, country=country, city=city, phone=phone, address=address,pin=pin,date_joined=date_joined)
+            return JsonResponse({'message': 'User created successfully'},status=201)
+        except Exception as e:
+            return JsonResponse({'error': e}, status=500) 
+        
+     else:
+            return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+
+@csrf_exempt
+def login(request):    
+    if request.method == 'POST':
+        data = json.loads(request.body)
+        email = data.get('email')
+        password = data.get('password')
+        try:
+            user = Auth.objects.get(email=email)
+            if user.password == password:
+                return JsonResponse({'message': 'Login successful'})
+            else:
+                return JsonResponse({'error': 'Wrong password'}, status=401)
+        except Auth.DoesNotExist:
+            return JsonResponse({'error': 'User not found'}, status=404) 
+    else:
+        return JsonResponse({'error': 'Invalid request method'}, status=400)   
+
+def forgot(request):
+    return JsonResponse({'message': list(Auth.objects.all().values())})
