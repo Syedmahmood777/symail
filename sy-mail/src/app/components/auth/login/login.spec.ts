6b8f8e5910bed671db23f13d9c5ef053f61b4ef2
syed <syedmahmood717@gmail.com>
2025-10-27 17:03:38 +0530

    added modules for testing in auth service and login module

--- sy-mail/src/app/components/auth/login/login.spec.ts
+++ sy-mail/src/app/components/auth/login/login.spec.ts
@@ -1,14 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { provideRouter } from '@angular/router';
 import { Login } from './login';
-
+import { HttpClientModule } from '@angular/common/http';
 describe('Login', () => {
   let component: Login;
   let fixture: ComponentFixture<Login>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [Login],
+      imports: [Login,HttpClientModule],
       providers: [provideRouter([])],
     }).compileComponents();
 
